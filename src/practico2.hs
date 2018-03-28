f4128d5159448e8aaedafb1b09a6527c75c91686
Svartskogen <tratex98@hotmail.com>
2018-03-28 14:58:04 -0300

    Ejercicio 3 terminado

--- src/practico2.hs
+++ src/practico2.hs
@@ -1,5 +1,20 @@
 --Ejercicio 3  
+-- a)
 soloPares :: [Int] -> [Int]
 soloPares [] = []
 soloPares (x:xs) | (mod x 2 == 0) = x:soloPares xs
                  | (mod x 2 /= 0) = soloPares xs
+
+-- b)
+mayoresQue10 :: [Int] -> [Int]
+mayoresQue10 [] = []
+mayoresQue10 (x:xs) | (x>10) = x:mayoresQue10 xs
+                    | (x<10) = mayoresQue10 xs
+
+-- c) 
+mayoresQue :: Int -> [Int] -> [Int]
+mayoresQue a [] = []
+mayoresQue a (x:xs) | (x>a) = x:mayoresQue a xs
+                    | True = mayoresQue a xs
+                   
+
